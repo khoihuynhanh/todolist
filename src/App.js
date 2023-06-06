da2bea57932fa3b07f43a04dcb87380b40f32b20
unknown <khoihuynh.github@gmail.com>
2023-06-06 11:42:43 +0700

    step2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [work, setWork] = useState('')
+  console.log(work);
+
   return (
-    <div>App</div>
+    <div className="flex items-center justify-center h-screen gap-8">
+      <input 
+        type="text"
+        className='outline-none border border-blue-600 px-4 py-2 w-80'
+        value={work}
+        onChange={e => setWork(e.target.value)}
+      />
+
+      <button
+        type="button"
+        className='outline-none px-4 py-2 bg-blue-500 rounded-md text-white'
+      >
+        ADD
+      </button>
+    </div>
   )
 }
 
